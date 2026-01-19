f85863375651f07cfdd23061c7288cb3c5307bdb
Mayuresh Muley <mayureshmuley@Mayureshs-MacBook-Pro.local>
2026-01-19 12:19:35 -0800

    change redis client

--- backend/app/redis_client.py
+++ backend/app/redis_client.py
@@ -1,14 +1,15 @@
 import os
 import redis
 from urllib.parse import urlparse
-
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("redis_client")
 # Get Redis connection URL
 REDIS_URL = os.environ.get("REDIS_URL", "redis://localhost:6379")  # fallback for local
 
-# Parse the URL
-parsed = urlparse(REDIS_URL)
+logger.info(f"Using Redis URL: {REDIS_URL}")
 
 redis_client = redis.StrictRedis.from_url(
     REDIS_URL,
-    decode_responses=False
+    decode_responses=True
 )
